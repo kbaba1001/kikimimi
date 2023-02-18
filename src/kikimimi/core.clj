f1d9cfbf15a03b6a32451c414b81501187e6c396
kbaba1001 <kbaba1001@users.noreply.github.com>
2023-02-18 16:17:44 +0900

    feat: add http server

--- src/kikimimi/core.clj
+++ src/kikimimi/core.clj
@@ -1,7 +1,14 @@
 (ns kikimimi.core
+  (:require [aleph.http :as http])
   (:gen-class))
+
+(defn handler [req]
+  {:status 200
+   :headers {"content-type" "text/plain"}
+   :body "hello!"})
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
+  (http/start-server handler {:port 8080})
   (println "Hello, World!"))
